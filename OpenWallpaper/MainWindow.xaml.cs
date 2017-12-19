9f4546babaa508f46445fc3d61982a6971d78ead
JunWang <jstzwj@aliyun.com>
2017-12-20 00:24:52 +0800

    添加项目文件。

---
+++ OpenWallpaper/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace OpenWallpaper
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IntPtr _handle;
+        private WindowState ws;
+        private WindowState wsl;
+        private NotifyIcon notifyIcon;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            icon();
+        }
+
+        private void icon()
+        {
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.Icon = new System.Drawing.Icon(@"AppIcon.ico");
+            this.notifyIcon.Visible = true;
+            //打开菜单项
+            System.Windows.Forms.MenuItem open = new System.Windows.Forms.MenuItem("Settings");
+            open.Click += new EventHandler(Show);
+            //退出菜单项
+            System.Windows.Forms.MenuItem exit = new System.Windows.Forms.MenuItem("Exit");
+            exit.Click += new EventHandler(Close);
+            //关联托盘控件
+            System.Windows.Forms.MenuItem[] childen = new System.Windows.Forms.MenuItem[] { open, exit };
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(childen);
+
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
+            {
+                if (e.Button == MouseButtons.Left) this.Show(o, e);
+            });
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Visible;
+            this.ShowInTaskbar = true;
+            this.Activate();
+        }
+
+        private void Hide(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            _handle = (new WindowInteropHelper(this)).Handle;
+            WindowDesktopBind.SetDesktopAsParent(_handle);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            double x1 = SystemParameters.PrimaryScreenWidth;//得到屏幕宽度
+            double y1 = SystemParameters.PrimaryScreenHeight;//得到屏幕高度
+            this.Left = 0;
+            this.Top = 0;
+            this.Height = y1;
+            this.Width = x1;
+            base.OnRender(drawingContext);
+        }
+    }
+}
---
+++ OpenWallpaper/WindowDesktopBind.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWallpaper
+{
+    public class WindowDesktopBind
+    {
+        static void PrintVisibleWindowHandles(int maxLevel = -1)
+        {
+            // Enumerates all existing top window handles. This includes open and visible windows, as well as invisible windows.
+            Win32Wrapper.EnumWindows(new Win32Wrapper.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                PrintVisibleWindowHandles(tophandle, maxLevel);
+                return true;
+            }), IntPtr.Zero);
+        }
+
+        static void PrintVisibleWindowHandles(IntPtr hwnd, int maxLevel = -1, int level = 0)
+        {
+            bool isVisible = Win32Wrapper.IsWindowVisible(hwnd);
+
+            if (isVisible && (maxLevel == -1 || level <= maxLevel))
+            {
+                StringBuilder className = new StringBuilder(256);
+                Win32Wrapper.GetClassName(hwnd, className, className.Capacity);
+
+                StringBuilder windowTitle = new StringBuilder(256);
+                Win32Wrapper.GetWindowText(hwnd, windowTitle, className.Capacity);
+
+                Console.WriteLine("".PadLeft(level * 2) + "0x{0:X8} \"{1}\" {2}", hwnd.ToInt64(), windowTitle, className);
+
+                level++;
+
+                // Enumerates all child windows of the current window
+                Win32Wrapper.EnumChildWindows(hwnd, new Win32Wrapper.EnumWindowsProc((childhandle, childparamhandle) =>
+                {
+                    PrintVisibleWindowHandles(childhandle, maxLevel, level);
+                    return true;
+                }), IntPtr.Zero);
+            }
+        }
+
+        public static void SetDesktopAsParent(IntPtr handle)
+        {
+            PrintVisibleWindowHandles(2);
+            // The output will look something like this. 
+            // .....
+            // 0x00010190 "" WorkerW
+            //   ...
+            //   0x000100EE "" SHELLDLL_DefView
+            //     0x000100F0 "FolderView" SysListVieWin32Wrapper
+            // 0x000100EC "Program Manager" Progman
+
+
+
+            // Fetch the Progman window
+            IntPtr progman = Win32Wrapper.FindWindow("Progman", null);
+
+            IntPtr result = IntPtr.Zero;
+
+            // Send 0x052C to Progman. This message directs Progman to spawn a 
+            // WorkerW behind the desktop icons. If it is already there, nothing 
+            // happens.
+            Win32Wrapper.SendMessageTimeout(progman,
+                                   0x052C,
+                                   new IntPtr(0),
+                                   IntPtr.Zero,
+                                   Win32Wrapper.SendMessageTimeoutFlags.SMTO_NORMAL,
+                                   1000,
+                                   out result);
+
+
+            PrintVisibleWindowHandles(2);
+            // The output will look something like this
+            // .....
+            // 0x00010190 "" WorkerW
+            //   ...
+            //   0x000100EE "" SHELLDLL_DefView
+            //     0x000100F0 "FolderView" SysListVieWin32Wrapper
+            // 0x00100B8A "" WorkerW                                   <--- This is the WorkerW instance we are after!
+            // 0x000100EC "Program Manager" Progman
+
+            IntPtr workerw = IntPtr.Zero;
+
+            // We enumerate all Windows, until we find one, that has the SHELLDLL_DefView 
+            // as a child. 
+            // If we found that window, we take its next sibling and assign it to workerw.
+            Win32Wrapper.EnumWindows(new Win32Wrapper.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = Win32Wrapper.FindWindowEx(tophandle,
+                                            IntPtr.Zero,
+                                            "SHELLDLL_DefView",
+                                            IntPtr.Zero);
+
+                if (p != IntPtr.Zero)
+                {
+                    // Gets the WorkerW Window after the current one.
+                    workerw = Win32Wrapper.FindWindowEx(IntPtr.Zero,
+                                               tophandle,
+                                               "WorkerW",
+                                               IntPtr.Zero);
+                    return false;
+                }
+
+                return true;
+            }), IntPtr.Zero);
+
+            Win32Wrapper.SetParent(handle, workerw);
+        }
+    }
+}
