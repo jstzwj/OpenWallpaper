23f02ea49641c0a1a50a40e2d75a37720a002ddb
JunWang <jstzwj@aliyun.com>
2017-12-20 19:42:27 +0800

    wallpaper reader

---
+++ OpenWallpaper/Settings/AppSettings.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWallpaper.Settings
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class AppSettings
+    {
+        // server
+        [JsonProperty(PropertyName = "name")]
+        [DefaultValue("User")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "autoStart")]
+        [DefaultValue(true)]
+        public bool AutoStart { get; set; }
+
+        [JsonProperty(PropertyName = "wallpaperManifest")]
+        [DefaultValue("wallpaperManifest.json")]
+        public string WallpaperManifest { get; set; }
+    }
+}
---
+++ OpenWallpaper/Wallpapers/WallpaperManifest.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWallpaper.Wallpapers
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class WallpaperManifestItem
+    {
+        [JsonProperty(PropertyName = "wallpaperName")]
+        [DefaultValue("default")]
+        public string WallpaperName { get; set; }
+
+        [JsonProperty(PropertyName = "wallpaperPath")]
+        [DefaultValue("/")]
+        public string WallpaperPath { get; set; }
+
+
+        [JsonProperty(PropertyName = "wallpaperType")]
+        [DefaultValue("web")]
+        public string wallpaperType { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptOut)]
+    public class WallpaperManifest
+    {
+        public List<WallpaperManifestItem> list;
+
+        public WallpaperManifest()
+        {
+            list = new List<WallpaperManifestItem>();
+        }
+
+        public static WallpaperManifest GetWallpapersList(string jsonData)
+        {
+            List<WallpaperManifestItem> list = new List<WallpaperManifestItem>();
+            JObject obj = JObject.Parse(jsonData);
+            JArray data = (JArray)obj["wallpapers"];
+            if (data != null && data.Count > 0)
+            {
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<WallpaperManifestItem>(item.ToString()));
+                }
+            }
+
+            WallpaperManifest wallpaperManifest = new WallpaperManifest();
+            wallpaperManifest.list = list;
+            return wallpaperManifest;
+        }
+    }
+}
